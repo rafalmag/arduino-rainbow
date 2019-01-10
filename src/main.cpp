c94e0cccbf3e14797802ed867e736345e85834ab
Rafal Magda <rafalmag@gmail.com>
2019-01-10 22:04:43 +0100

    new rainbow and night added

--- src/main.cpp
+++ src/main.cpp
@@ -25,13 +25,21 @@ uint8_t max_bright = 128;
 // Initialize LED array.
 struct CRGB leds[NUM_LEDS];
 
-// Colours defined for below
-long firstval = 0xff00ff;
-CRGB rgbval(50, 0, 500);
-CHSV hsvval(100, 255, 200);
-
 uint8_t startpos = 0;
 int endpos = NUM_LEDS - 1;
+
+CRGB b = CRGB::Black;
+CRGB w(20,20,20);
+CRGBPalette16 nightPalette(b, b, b, w, b, b, b, w, b, b, b, w, b, b, b, w);
+
+void fillLedsFromPaletteColors(CRGBPalette16 targetPalette)
+{
+
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
+    leds[i] = ColorFromPalette(targetPalette, i + sin8(i * 16), 255);
+  }
+}
 
 void setup()
 {
@@ -63,8 +71,20 @@ void loop()
     fill_solid(leds, NUM_LEDS, 0);
     break;
   case 1:
+    Serial.println("cloud");
+    fillLedsFromPaletteColors(CloudColors_p);
+    break;
+  case 2:
     Serial.println("rainbow");
     fill_rainbow(leds, NUM_LEDS, initialHue, deltahue);
+    break;
+  case 3:
+    Serial.println("rainbow2");
+    fill_rainbow(leds, NUM_LEDS, initialHue, 15);
+    break;
+  case 4:
+    Serial.println("night");
+    fillLedsFromPaletteColors(nightPalette);
     break;
   default:
     // will be incremented later to 0
@@ -75,28 +95,4 @@ void loop()
   delay(2000);
   mode = mode + 1;
   Serial.println(mode);
-
-  // fill_solid section
-  //  fill_solid(leds,NUM_LEDS, firstval);                        // A 'long' RGB value
-  //  fill_solid(leds, NUM_LEDS, CRGB::HotPink);                  // https://github.com/FastLED/FastLED/wiki/Pixel-reference#predefined-colors-list
-  //  fill_solid(leds, NUM_LEDS, rgbval);                         // 8 bit values inside the colour definition
-  //  fill_solid(leds, NUM_LEDS, hsvval);                         // 8 bit values inside the colour definition
-
-  // fill_solid(leds, 5, rgbval);
-  // fill_solid(leds+5,5, CRGB::Green);
-  // fill_solid(leds+10,5, hsvval);
-
-  // fill_gradient section
-  //  fill_gradient_RGB(leds, startpos, 0x000011, endpos, 0x110000);   // You can mix and match long values and CRGB values. Remember, endpos goes up to NUM_LEDS-1
-  //  fill_gradient_RGB(leds, NUM_LEDS, CRGB(50,0,200), CRGB(80,200,240));  // up to 4 CRGB (or long) values
-
-  //FORWARD_HUES, BACKWARD_HUES, SHORTEST_HUES, LONGEST_HUES
-  //  fill_gradient(leds, startpos, CHSV(50, 255,255) , endpos, CHSV(150,255,255), SHORTEST_HUES);
-  //  fill_gradient(leds, NUM_LEDS, CHSV(50, 255,255), CHSV(100,255,255), LONGEST_HUES);    // up to 4 CHSV values
-
-  // fill_rainbow section
-  //  fill_rainbow(leds, NUM_LEDS, thishue, deltahue);            // Use FastLED's fill_rainbow routine.
-  // fill_solid(leds, NUM_LEDS, 0); // Clear the strip for. . .
-  //  fill_rainbow(leds+1, NUM_LEDS-2, thishue, deltahue);        // One pixel border at each end.
-
 } // loop()
