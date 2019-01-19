c41fc676753030adbdc314dca67168adb692495f
Rafal Magda <rafalmag@gmail.com>
2019-01-19 13:50:10 +0100

    encoder WIP and brightness set

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,14 @@
 #define LED_TYPE NEOPIXEL
 #define NUM_LEDS 60
 
+#define encoder0PinA 9
+#define encoder0PinB 10
+#define encoder0Button 11
+
+int encoder0Pos = 0;
+int encoder0PinALast = LOW;
+int n = LOW;
+
 // Global variables can be changed on the fly.
 // Overall brightness definition. It can be changed on the fly.
 uint8_t max_bright = 128;
@@ -29,7 +37,7 @@ uint8_t startpos = 0;
 int endpos = NUM_LEDS - 1;
 
 CRGB b = CRGB::Black;
-CRGB w(20,20,20);
+CRGB w(20, 20, 20);
 CRGBPalette16 nightPalette(b, b, b, w, b, b, b, w, b, b, b, w, b, b, b, w);
 
 void fillLedsFromPaletteColors(CRGBPalette16 targetPalette)
@@ -46,10 +54,16 @@ void setup()
   Serial.begin(9600); // Initialize serial port for debugging.
   delay(1000);        // Soft startup to ease the flow of electrons.
 
+  pinMode(encoder0PinA, INPUT_PULLUP);
+  pinMode(encoder0PinB, INPUT_PULLUP);
+  pinMode(encoder0Button, INPUT_PULLUP);
+
   LEDS.addLeds<LED_TYPE, DATA_PIN>(leds, NUM_LEDS);
 
   FastLED.setBrightness(max_bright);
-  set_max_power_in_volts_and_milliamps(5, 5000); // FastLED Power management set at 5V, 500mA.
+  set_max_power_in_volts_and_milliamps(5, 5000); // FastLED Power management set at 5V, 5000mA.
+
+  analogReference(INTERNAL); //1.1V
 }
 
 int mode = 0;
@@ -63,27 +77,49 @@ uint8_t deltahue = 270 / NUM_LEDS;
 
 void loop()
 {
+  //encoder stuff
+  n = digitalRead(encoder0PinA);
+  if ((encoder0PinALast == LOW) && (n == HIGH))
+  {
+    if (digitalRead(encoder0PinB) == LOW)
+    {
+      encoder0Pos--;
+    }
+    else
+    {
+      encoder0Pos++;
+    }
+    Serial.print(encoder0Pos);
+    Serial.print(",");
+  }
+  encoder0PinALast = n;
+
+  // brightness
+  // div by 4 to convert 0-1023 to 0-255
+  max_bright = analogRead(3) / 4;
+  FastLED.setBrightness(max_bright);
+
   switch (mode)
   {
   case 0:
-    Serial.println("clear");
+    // Serial.println("clear");
     // Clear the strip
     fill_solid(leds, NUM_LEDS, 0);
     break;
   case 1:
-    Serial.println("cloud");
+    // Serial.println("cloud");
     fillLedsFromPaletteColors(CloudColors_p);
     break;
   case 2:
-    Serial.println("rainbow");
+    // Serial.println("rainbow");
     fill_rainbow(leds, NUM_LEDS, initialHue, deltahue);
     break;
   case 3:
-    Serial.println("rainbow2");
+    // Serial.println("rainbow2");
     fill_rainbow(leds, NUM_LEDS, initialHue, 15);
     break;
   case 4:
-    Serial.println("night");
+    // Serial.println("night");
     fillLedsFromPaletteColors(nightPalette);
     break;
   default:
@@ -92,7 +128,10 @@ void loop()
     break;
   }
   FastLED.show(); // Power managed display
-  delay(2000);
-  mode = mode + 1;
-  Serial.println(mode);
+  // delay(2000);
+  EVERY_N_MILLISECONDS(2000)
+  {
+    mode = mode + 1;
+    Serial.println(mode);
+  }
 } // loop()
